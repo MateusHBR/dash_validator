5c033b132c19f1ecaa0695951662ecd225be23e1
MateusHBR <mateusmorais.r@gmail.com>
2021-09-09 22:39:32 -0300

    add email to group of tests

--- test/src/validations/email_dash_validation/email_dash_validation_test.dart
+++ test/src/validations/email_dash_validation/email_dash_validation_test.dart
@@ -1,38 +1,41 @@
-import 'package:dash_validator/src/validations/email_dash_validation/email_dash_validation.dart';
 import 'package:test/test.dart';
 
+import 'package:dash_validator/src/validations/email_dash_validation/email_dash_validation.dart';
+
 void main() {
-  test('should return true when field is null or empty', () async {
-    String? sut = '';
-    bool isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, true);
+  group('Email validation', () {
+    test('should return true when field is null or empty', () async {
+      String? sut = '';
+      bool isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, true);
 
-    sut = null;
-    isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, true);
-  });
+      sut = null;
+      isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, true);
+    });
 
-  test('should return true when email is valid', () async {
-    String? sut = 'mateushbr@email.com';
-    bool isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, true);
+    test('should return true when email is valid', () async {
+      String? sut = 'mateushbr@email.com';
+      bool isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, true);
 
-    sut = 'valid_email@flutter.com.br';
-    isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, true);
-  });
+      sut = 'valid_email@flutter.com.br';
+      isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, true);
+    });
 
-  test('should return false when email is invalid valid', () async {
-    String? sut = 'mateushbr@email.';
-    bool isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, false);
+    test('should return false when email is invalid valid', () async {
+      String? sut = 'mateushbr@email.';
+      bool isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, false);
 
-    sut = '@flutter.com.br';
-    isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, false);
+      sut = '@flutter.com.br';
+      isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, false);
 
-    sut = 'flutter';
-    isValid = EmailDashValidation().isValid(sut);
-    expect(isValid, false);
+      sut = 'flutter';
+      isValid = EmailDashValidation().isValid(sut);
+      expect(isValid, false);
+    });
   });
 }
